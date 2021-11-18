3b77bedf69d55fde58b1ddad53323d8c1e21e09c
johnmccants002 <johnmccants002@gmail.com>
2021-11-18 15:50:02 -0800

    Added Login Screen

---
+++ DevParty/DevParty/Extensions.swift
@@ -0,0 +1,291 @@
+//
+//  Extensions.swift
+//  DevParty
+//
+//  Created by John McCants on 11/18/21.
+//
+
+import Foundation
+import UIKit
+
+public enum SimpleAnimationEdge {
+  case none
+  case top
+  case bottom
+  case left
+  case right
+}
+
+extension UIView {
+    func anchor(top: NSLayoutYAxisAnchor? = nil,
+                left: NSLayoutXAxisAnchor? = nil,
+                bottom: NSLayoutYAxisAnchor? = nil,
+                right: NSLayoutXAxisAnchor? = nil,
+                paddingTop: CGFloat = 0,
+                paddingLeft: CGFloat = 0,
+                paddingBottom: CGFloat = 0,
+                paddingRight: CGFloat = 0,
+                width: CGFloat? = nil,
+                height: CGFloat? = nil) {
+        
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        if let top = top {
+            topAnchor.constraint(equalTo: top, constant: paddingTop).isActive = true
+        }
+        
+        if let left = left {
+            leftAnchor.constraint(equalTo: left, constant: paddingLeft).isActive = true
+        }
+        
+        if let bottom = bottom {
+            bottomAnchor.constraint(equalTo: bottom, constant: -paddingBottom).isActive = true
+        }
+        
+        if let right = right {
+            rightAnchor.constraint(equalTo: right, constant: -paddingRight).isActive = true
+        }
+        
+        if let width = width {
+            widthAnchor.constraint(equalToConstant: width).isActive = true
+        }
+        
+        if let height = height {
+            heightAnchor.constraint(equalToConstant: height).isActive = true
+        }
+    }
+    
+    func center(inView view: UIView, yConstant: CGFloat? = 0) {
+        translatesAutoresizingMaskIntoConstraints = false
+        centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: yConstant!).isActive = true
+    }
+    
+    func centerX(inView view: UIView, topAnchor: NSLayoutYAxisAnchor? = nil, paddingTop: CGFloat? = 0) {
+        translatesAutoresizingMaskIntoConstraints = false
+        centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        
+        if let topAnchor = topAnchor {
+            self.topAnchor.constraint(equalTo: topAnchor, constant: paddingTop!).isActive = true
+        }
+    }
+    
+    func centerY(inView view: UIView, leftAnchor: NSLayoutXAxisAnchor? = nil, paddingLeft: CGFloat? = nil, constant: CGFloat? = 0) {
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: constant!).isActive = true
+        
+        if let leftAnchor = leftAnchor, let padding = paddingLeft {
+            self.leftAnchor.constraint(equalTo: leftAnchor, constant: padding).isActive = true
+        }
+    }
+    
+    func setDimensions(width: CGFloat, height: CGFloat) {
+        translatesAutoresizingMaskIntoConstraints = false
+        widthAnchor.constraint(equalToConstant: width).isActive = true
+        heightAnchor.constraint(equalToConstant: height).isActive = true
+    }
+    
+    func addConstraintsToFillView(_ view: UIView) {
+        translatesAutoresizingMaskIntoConstraints = false
+        anchor(top: view.topAnchor, left: view.leftAnchor,
+               bottom: view.bottomAnchor, right: view.rightAnchor)
+    }
+}
+
+extension UIView {
+    func setRoundedView() {
+        self.layer.cornerRadius = (self.frame.width / 25)
+        self.layer.masksToBounds = true
+        self.contentMode = .scaleAspectFill
+    }
+    
+}
+
+extension UIImageView {
+
+    func setRounded() {
+        self.layer.cornerRadius = (self.frame.width / 2)
+        self.layer.masksToBounds = true
+        self.contentMode = .scaleAspectFill
+    }
+}
+
+
+extension UIView {
+    public var viewWidth: CGFloat {
+        return self.frame.size.width
+    }
+
+    public var viewHeight: CGFloat {
+        return self.frame.size.height
+    }
+}
+
+extension UIButton {
+    func alignTextBelow(spacing: CGFloat) {
+        guard let image = self.imageView?.image else {
+            return
+        }
+        print("passed 1st guard")
+        guard let titleLabel = self.titleLabel else {
+            return
+        }
+        print("passed 2nd guard")
+        guard let titleText = titleLabel.text else {
+            return
+        }
+        print("passed 3rd guard")
+        let titleSize = titleText.size(withAttributes: [
+            NSAttributedString.Key.font: titleLabel.font
+        ])
+        
+        titleEdgeInsets = UIEdgeInsets(top: spacing, left: -image.size.width, bottom: -image.size.height, right: 0)
+        imageEdgeInsets = UIEdgeInsets(top: -(titleSize.height + spacing), left: 0, bottom: 0, right: -titleSize.width)
+    }
+}
+
+
+extension UIView {
+    @discardableResult func slideIn(from edge: SimpleAnimationEdge = .none,
+                                    x: CGFloat = 0,
+                                    y: CGFloat = 0,
+                                    duration: TimeInterval = 0.4,
+                                    delay: TimeInterval = 0,
+                                    completion: ((Bool) -> Void)? = nil) -> UIView {
+      let offset = offsetFor(edge: edge)
+      transform = CGAffineTransform(translationX: offset.x + x, y: offset.y + y)
+      isHidden = false
+      UIView.animate(
+        withDuration: duration, delay: delay, usingSpringWithDamping: 1, initialSpringVelocity: 2,
+        options: .curveEaseOut, animations: {
+          self.transform = .identity
+          self.alpha = 1
+        }, completion: completion)
+      return self
+    }
+    
+    private func offsetFor(edge: SimpleAnimationEdge) -> CGPoint {
+      if let parentSize = self.superview?.frame.size {
+        switch edge {
+        case .none: return CGPoint.zero
+        case .top: return CGPoint(x: 0, y: -frame.maxY)
+        case .bottom: return CGPoint(x: 0, y: parentSize.height - frame.minY)
+        case .left: return CGPoint(x: -frame.maxX, y: 0)
+        case .right: return CGPoint(x: parentSize.width - frame.minX, y: 0)
+        }
+      }
+      return .zero
+    }
+}
+
+extension UIView {
+    enum GlowEffect: Float {
+        case small = 0.4, normal = 2, big = 50
+    }
+
+    func doGlowAnimation(withColor color: UIColor, withEffect effect: GlowEffect = .normal) {
+        layer.masksToBounds = false
+        layer.shadowColor = color.cgColor
+        layer.shadowRadius = 1
+        layer.shadowOpacity = 1.5
+        layer.shadowOffset = .zero
+
+        let glowAnimation = CABasicAnimation(keyPath: "shadowRadius")
+        glowAnimation.fromValue = 0
+        glowAnimation.toValue = effect.rawValue
+        glowAnimation.beginTime = CACurrentMediaTime()+0.3
+        glowAnimation.duration = CFTimeInterval(0.3)
+        glowAnimation.fillMode = .removed
+        
+
+        glowAnimation.autoreverses = true
+        glowAnimation.isRemovedOnCompletion = true
+        layer.add(glowAnimation, forKey: "shadowGlowingAnimation")
+    }
+}
+
+extension UIColor {
+    static func rgb(red: CGFloat, green: CGFloat, blue: CGFloat) -> UIColor {
+        return UIColor(red: red/255, green: green/255, blue: blue/255, alpha: 1)
+    }
+    
+    static let twitterBlue = UIColor.rgb(red: 29, green: 161, blue: 242)
+}
+
+extension UIViewController {
+
+    @objc func hideKeyboardWhenTappedAround() {
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action:    #selector(UIViewController.dismissKeyboard))
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+    }
+
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
+}
+
+extension UITapGestureRecognizer {
+    func didTapAttributedTextInLabel(label: UILabel, targetText: String) -> Bool {
+        guard let attributedString = label.attributedText, let lblText = label.text else { return false }
+        print("This is the length \(attributedString.length)")
+        let targetRange = (lblText as NSString).range(of: targetText)
+        //IMPORTANT label correct font for NSTextStorage needed
+        let mutableAttribString = NSMutableAttributedString(attributedString: attributedString)
+        mutableAttribString.addAttributes(
+            [NSAttributedString.Key.font: label.font ?? UIFont.smallSystemFontSize],
+            range: NSRange(location: 0, length: attributedString.length)
+        )
+        // Create instances of NSLayoutManager, NSTextContainer and NSTextStorage
+        let layoutManager = NSLayoutManager()
+        let textContainer = NSTextContainer(size: CGSize.zero)
+        let textStorage = NSTextStorage(attributedString: mutableAttribString)
+
+        // Configure layoutManager and textStorage
+        layoutManager.addTextContainer(textContainer)
+        textStorage.addLayoutManager(layoutManager)
+
+        // Configure textContainer
+        textContainer.lineFragmentPadding = 0.0
+        textContainer.lineBreakMode = label.lineBreakMode
+        textContainer.maximumNumberOfLines = label.numberOfLines
+        let labelSize = label.intrinsicContentSize
+        textContainer.size = labelSize
+
+        // Find the tapped character location and compare it to the specified range
+        let locationOfTouchInLabel = self.location(in: label)
+        let textBoundingBox = layoutManager.usedRect(for: textContainer)
+        let textContainerOffset = CGPoint(x: (labelSize.width - textBoundingBox.size.width) * 0.5 - textBoundingBox.origin.x,
+                                          y: (labelSize.height - textBoundingBox.size.height) * 0.5 - textBoundingBox.origin.y);
+        let locationOfTouchInTextContainer = CGPoint(x: locationOfTouchInLabel.x - textContainerOffset.x, y:
+            locationOfTouchInLabel.y - textContainerOffset.y);
+        let indexOfCharacter = layoutManager.characterIndex(for: locationOfTouchInTextContainer, in: textContainer, fractionOfDistanceBetweenInsertionPoints: nil)
+
+        return NSLocationInRange(indexOfCharacter, targetRange)
+    }
+
+}
+
+extension CALayer {
+
+func addBorder(edge: UIRectEdge, color: UIColor, thickness: CGFloat) {
+
+    let border = CALayer()
+
+    switch edge {
+    case .top:
+        border.frame = CGRect(x: 0, y: 0, width: frame.width, height: thickness)
+    case .bottom:
+        border.frame = CGRect(x: 0, y: frame.height - thickness, width: frame.width, height: thickness)
+    case .left:
+        border.frame = CGRect(x: 0, y: 0, width: thickness, height: frame.height)
+    case .right:
+        border.frame = CGRect(x: frame.width - thickness, y: 0, width: thickness, height: frame.height)
+    default:
+        break
+    }
+    border.backgroundColor = color.cgColor
+    addSublayer(border)
+    }
+}
--- DevParty/DevParty/ViewController.swift
+++ DevParty/DevParty/ViewController.swift
@@ -6,14 +6,76 @@
 //
 
 import UIKit
+import AVKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var createAccountButton: UIButton!
+    @IBOutlet weak var signInButton: UIButton!
+    var avPlayer: AVPlayer!
+    var avPlayerLayer: AVPlayerLayer!
+    var paused: Bool = false
+    
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        guard let theURL = Bundle.main.url(forResource:"dev", withExtension: "mov") else { return }
+
+        
+        avPlayer = AVPlayer(url: theURL)
+        avPlayerLayer = AVPlayerLayer(player: avPlayer)
+        avPlayerLayer.videoGravity = .resizeAspectFill
+        avPlayer.volume = 0
+        avPlayer.actionAtItemEnd = .none
+
+        avPlayerLayer.frame = view.layer.bounds
+        view.backgroundColor = .clear
+        view.layer.insertSublayer(avPlayerLayer, at: 0)
+
+        NotificationCenter.default.addObserver(self,
+                                           selector: #selector(playerItemDidReachEnd(notification:)),
+                                           name: .AVPlayerItemDidPlayToEndTime,
+                                           object: avPlayer.currentItem)
     }
 
+    @objc func playerItemDidReachEnd(notification: Notification) {
+        let p: AVPlayerItem = notification.object as! AVPlayerItem
+        p.seek(to: .zero)
+    }
 
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        avPlayer.play()
+        paused = false
+    }
+
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        avPlayer.pause()
+        paused = true
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        updateViews()
+    }
+    
+    // MARK: - Helper Functions
+    
+    func updateViews() {
+        
+        // Create Account Button
+        createAccountButton.backgroundColor = .twitterBlue
+        createAccountButton.layer.cornerRadius = createAccountButton.bounds.height / 2
+        createAccountButton.setTitleColor(UIColor.white, for: .normal)
+//
+//
+//        // Sign In Button
+//        signInButton.setTitleColor(UIColor.white, for: .normal)
+        
+    
+      
+    }
+    
 }
 
